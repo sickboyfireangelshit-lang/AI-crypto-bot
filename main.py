3933bdfd690eecd0ed676801ba93ce7128ccd916
sickboyfireangelshit-lang <sickboyfireangelshit@gmail.com>
2025-12-21 01:44:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -224,4 +224,3 @@ async def background_trading_swarm():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
-
