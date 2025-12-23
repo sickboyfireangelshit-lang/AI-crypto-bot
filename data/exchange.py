e417d72d9f66bfb4501cef004230897282bfee4d
sickboyfireangelshit-lang <sickboyfireangelshit@gmail.com>
2025-12-22 18:04:34 -0800

    Create exchange.py

---
+++ data/exchange.py
@@ -0,0 +1,10 @@
+"""Exchange Data Module"""
+import logging
+logger = logging.getLogger(__name__)
+
+class ExchangeData:
+    def __init__(self):
+        logger.info("ExchangeData initialized (stub)")
+        self.exchange = None
+
+exchange_data = ExchangeData()
