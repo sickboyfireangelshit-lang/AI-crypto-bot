111f7bf07064485a3b61989a75dc4613fac18afb
sickboyfireangelshit-lang <sickboyfireangelshit@gmail.com>
2025-12-27 14:23:47 -0800

    Update telegram_alerts.py

--- utils/utils/telegram_alerts.py
+++ utils/utils/telegram_alerts.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 class TelegramAlerts:
-    def __init__(self, token=None, chat_id=None):
+    def __init__(self, token="19713088194", chat_id="8144453170:AAG-VUiaO0EuNejPvE7X1Hlm4_ghbobWToI":
         self.enabled = False
         logger.warning("Telegram alerts disabled - no token provided")
     
